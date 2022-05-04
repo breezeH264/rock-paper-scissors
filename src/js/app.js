24d0a2bbd5582aa369507060dedc9f4dcbc77a6b
breezeH264 <breezeH264@gmail.com>
2022-05-04 16:31:45 -0700

    Add DOM Manipulation and remove console logs

--- src/js/app.js
+++ src/js/app.js
@@ -1,55 +1,115 @@
-function game() {
-  const choice = ["ROCK", "PAPER", "SCISSORS"];
+const rps = ["ROCK", "PAPER", "SCISSORS"];
 
-  function computerPlay() {
-    let selection = choice[Math.floor(Math.random() * choice.length)];
-    return selection;
+let computerPoints = 0;
+let playerPoints = 0;
+let round = 0;
+
+// picks computer's choice
+computerPicks = function () {
+  let randomChoice = rps[Math.floor(Math.random() * rps.length)];
+  return randomChoice;
+};
+
+// plays one round of rock, paper, scissors
+// compares player's and computer's selection to determine winner
+playRound = function (computerSelection, playerSelection) {
+  if (computerSelection === playerSelection) {
+    roundResult.textContent = "It's a tie! Try again.";
   }
 
-  let computerPoints = 0;
-  let playerPoints = 0;
-
-  function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-      console.log("It's a tie! No points have been awarded.");
-    }
-
-    if (
-      (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
-      (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
-      (computerSelection === "PAPER" && playerSelection === "ROCK")
-    ) {
-      console.log(
-        "Computer wins! " + computerSelection + " beats " + playerSelection
-      );
-      computerPoints++;
-    }
-    if (
-      (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
-      (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
-      (playerSelection === "PAPER" && computerSelection === "ROCK")
-    ) {
-      console.log(
-        "Player wins! " + playerSelection + " beats " + computerSelection
-      );
-      playerPoints++;
-    }
-  }
-  for (let i = 0; i < 5; i++) {
-    computerSelection = computerPlay();
-    playerSelection = prompt("Rock, paper, or scissors?").toUpperCase();
-    playRound(computerSelection, playerSelection);
-  }
-  if (computerPoints > playerPoints) {
-    console.log("GAME OVER! Computer wins the game!");
-  } else if (playerPoints > computerPoints) {
-    console.log("GAME OVER! Player wins the game!");
-  } else {
-    console.log("Tie! You both fucking suck!");
+  if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+  ) {
+    computerPoints++;
+    computerScore.textContent = "Computer: " + computerPoints;
+    roundResult.textContent =
+      computerSelection +
+      " beats " +
+      playerSelection +
+      ". Computer wins this round!";
   }
 
-  console.log("Player: " + playerPoints);
-  console.log("Computer: " + computerPoints);
-}
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    playerPoints++;
+    playerScore.textContent = "Player: " + playerPoints;
+    roundResult.textContent =
+      playerSelection +
+      " beats " +
+      computerSelection +
+      ". Player wins this round!";
+  }
+  if (playerPoints === 5 || computerPoints === 5) {
+    gameResult.textContent = "Game over!";
+    btnRock.style.visibility = "hidden";
+    btnPaper.style.visibility = "hidden";
+    btnScissors.style.visibility = "hidden";
+    resetButton.style.visibility = "visible";
+  }
+};
 
-game();
+const btnRock = document.querySelector("#ROCK");
+btnRock.addEventListener("click", () => {
+  computerSelection = computerPicks();
+  playerSelection = "ROCK";
+  playRound(computerSelection, playerSelection);
+  round++;
+});
+
+const btnScissors = document.querySelector("#SCISSORS");
+btnScissors.addEventListener("click", () => {
+  computerSelection = computerPicks();
+  playerSelection = "SCISSORS";
+  playRound(computerSelection, playerSelection);
+  round++;
+});
+
+const btnPaper = document.querySelector("#PAPER");
+btnPaper.addEventListener("click", () => {
+  computerSelection = computerPicks();
+  playerSelection = "PAPER";
+  playRound(computerSelection, playerSelection);
+  round++;
+});
+
+const resetButton = document.querySelector("#RESET");
+resetButton.addEventListener("click", () => {
+  resetButton.style.visibility = "hidden";
+  btnRock.style.visibility = "visible";
+  btnPaper.style.visibility = "visible";
+  btnScissors.style.visibility = "visible";
+
+  playerPoints = 0;
+  computerPoints = 0;
+  playerScore.textContent = "Player: 0";
+  computerScore.textContent = "Computer: 0";
+  gameResult.textContent = "";
+  roundResult.textContent = "";
+});
+
+resetButton.style.visibility = "hidden";
+
+const container = document.querySelector(".container");
+
+playerScore = document.createElement("p");
+playerScore.classList.add("score");
+playerScore.textContent = "Player: 0";
+playerScore.style.color = "red";
+container.appendChild(playerScore);
+
+computerScore = document.createElement("p");
+computerScore.classList.add("score");
+computerScore.textContent = "Computer: 0";
+computerScore.style.color = "blue";
+container.appendChild(computerScore);
+
+roundResult = document.createElement("p");
+container.appendChild(roundResult);
+
+gameResult = document.createElement("p");
+container.appendChild(gameResult);
